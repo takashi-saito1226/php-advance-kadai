d050e5573942e096fa92736aab2066584b127404
takashi-saito1226 <139130594+takashi-saito1226@users.noreply.github.com>
2023-08-18 08:39:32 +0900

    Add files via upload

--- kadai_016/php-book-app/index.php
+++ kadai_016/php-book-app/index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="ja">
-
+ 
 <head>
-    <meta charset="UTF-8">
-    <title>書籍管理アプリ</title>
-    <link rel="stylesheet" href="style.css">
-
-    <!-- Google Fontsの読み込み -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>書籍管理アプリ</title>
+     <link rel="stylesheet" href="css/style.css">
+ 
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -19,16 +19,15 @@
         </nav>
     </header>
     <main>
-        <article class="home">
-            <h1>書籍管理アプリ</h1>
-            <p>『PHPとデータベースを連携しよう』成果物</p>
-            <a href="read.php" class="btn">書籍一覧</a>
-        </article>
+      <article class="home">
+        <h1>書籍管理アプリ</h1>
+        <p>『PHPとデータベースを連携しよう』成果物</p>
+        <a href="read.php" class="btn">書籍一覧</a>
+      </article>
     </main>
     <footer>
-        <p class="copyright">&copy; 書籍管理アプリ All rights reserved.</p>
+      <p class="copyright">&copy; 書籍管理アプリ All rights reserved.</p>
     </footer>
-
 </body>
 
 </html>
